9578a4c2a643b65e1a99271779e5a3c3b4a2f391
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-06-05 12:56:56 -0300

    Video 30

---
+++ jQuery/CodigoFacilito/peticion.php
@@ -0,0 +1,13 @@
+<?
+
+$nombre = $_POST['nombre'];
+$mensaje = $_POST['mensaje'];
+$mail = $_POST['mail'];
+
+if ($nombre != "" && $mensaje != "" && $mail != "") {
+    echo "Correcto";
+} else {
+    echo "Incorrecto";
+}
+
+?>
