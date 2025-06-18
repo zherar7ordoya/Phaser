bb5ba2df1eaefadd1fe93ba338f7373dc6e43ec9
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-06-18 09:18:52 -0300

    Cleaning...

--- webpack/000/src/index.js
+++
@@ -1,8 +0,0 @@
-function greet() {
-    console.log("Hello, World!");
-    const message = document.createElement('h1');
-    message.textContent = "Hello, World!";
-    document.body.appendChild(message);
-}
-
-greet();
