4643633e2f326c298f639bf1539dacaa8c82dea3
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-06-18 19:58:48 -0300

    Proyecto React

--- TypeScript/Sergie Code/2. React + TS/src/main.tsx
+++ TypeScript/Sergie Code/2. React + TS/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./styles.css"; // Importing a CSS file for styles
+import { TodoApp } from "./components/TodoApp";
+
+createRoot(document.getElementById("root")!).render
+(
+    <StrictMode>
+        <TodoApp />
+    </StrictMode>,
+);
